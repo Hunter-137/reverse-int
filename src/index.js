8fddab55ddd275c839916a5827fa91fec8481a3d
Hunter-137 <asrarkulovamal@gmail.com>
2023-08-19 02:10:25 +0500

    feat: npm install & add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let naturalCount = Math.abs(n);
+    let prepare = naturalCount.toString();
+    let i = 0;
+    let result = '';
+
+    while (i < prepare.length) {
+        result = `${prepare[i]}${result}`;
+        i = i + 1;
+    }
+    prepare = Math.abs(result);
+    return prepare;   
 }
